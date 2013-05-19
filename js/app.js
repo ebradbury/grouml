ba537ece092e37ffcd90b7911cafa1f67dd99302
Elliot Bradbury <ebradbury@gmail.com>
2013-05-19 01:55:18 -0400

    Added some rough code for issue #1. Needs work.

--- js/app.js
+++ js/app.js
@@ -19,17 +19,41 @@ GROUML.Generators = (function() {
     };
 })();
 
+GROUML.Core = (function() {
+    return {
+        addFieldToClass: function(fieldName, type, className) {
+            var classDiagram = GROUML.Collections.UmlObjects.findWhere({Name: className, Type: 'Class'});
+            
+            // Are UmlObjectFields tied to specific UmlObjects via FK yet?
+            var field = new GROUML.Models.UmlObjectField({ Name: field, Type: type });
+        },
+    };
+})();
+
 (function($){
     $(function() {
         var grid_size = 20;
 
         var m = new GROUML.Models.UmlObject({
-            Name: 'Person'
+            Name: 'Person',
+            Type: 'Class',
         });
 
         var c = new GROUML.Collections.UmlObjectFields([
             {
                 Name: 'id',
+                Type: 'int'
+            },
+            {
+                Name: 'last_name',
+                Type: 'char'
+            },
+            {
+                Name: 'last_name',
+                Type: 'char'
+            },
+            {
+                Name: 'age',
                 Type: 'int'
             }
         ]);
@@ -39,8 +63,24 @@ GROUML.Generators = (function() {
             collection: c
         });
 
-        $('body').append(Test.render().el);
+        $('#wall').append(Test.render().el);
 
+        var infinitedrag = $.infinitedrag("#wall", {}, {
+            width: 100,
+            height: 100,
+            start_col: 0,
+            start_row: 0,
+            oncreate: function(){},
+        });
+        
         $('div.uml-object').draggable({ grid: [grid_size, grid_size] }).resizable({ grid: [grid_size, grid_size] });
+        $('div.uml-object add-field').on('click', function() {
+            var className = $(this).closest('div.uml-object p.name').text();
+            
+            var field = new GROUML.Models.UmlObjectField({Name: '', Type: ''});
+            
+            // Unfinished. Need to re-render the view for the object so that new, blank field will show up.
+            Test.render().el;
+        });
     });
 })(jQuery);
