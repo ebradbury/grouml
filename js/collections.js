860d9379ad5e917101cdaf76fd32e03906bb970b
Elliot Bradbury <ebradbury@gmail.com>
2013-05-19 01:55:34 -0400

    Added UmlObject collection

--- js/collections.js
+++ js/collections.js
@@ -2,8 +2,13 @@ GROUML = GROUML || {};
 
 (function(c) {
 
+    c.UmlObjects = Backbone.Collection.extend({
+        model: GROUML.Models.UmlObject
+    });
+    
     c.UmlObjectFields = Backbone.Collection.extend({
         model: GROUML.Models.UmlObjectField
     });
 
+    
 })(GROUML.Collections = GROUML.Collections || {});
