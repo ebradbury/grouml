d12ab522125756994f69dbffc191de8220841de0
Elliot Bradbury <ebradbury@gmail.com>
2013-05-22 21:12:56 -0400

    incomplete generator changes - will be changed

--- js/generators/django.js
+++ js/generators/django.js
@@ -13,38 +13,16 @@
     var indent = '    ';
 
     var djangoGenerator = {
-        generate: function(name, obj) {
+        generate: function(obj) {
+            var name = obj.model.Name;
             var all = 'class ' + name + '(models.Model):\n';
-            for(var i in obj) {
-                if (!obj.hasOwnProperty(i)) {
-                    continue;
-                }
-
-                var v = obj[i];
-
-                if (typeof v === 'string') {
-                    all += u.camelToUnderscore(i) + ' = models.' + djangoTypeMapping[v] + '()\n';
-                } else {
-                    var f = u.camelToUnderscore(i) + ' = models.' + djangoTypeMapping[v['type']] + '(',
-                        count = 0;
-                    for (var p in v) {
-                        if (!v.hasOwnProperty(p)) {
-                            continue;
-                        }
-
-                        if (p !== 'type') {
-                            if (count++ !== 0) {
-                                f += ', ';
-                            }
-                            f += p + '=' + v[p];
-                        }
-                    }
-                    f += ')';
-                    all += indent + f + '\n';
-                }
-            }
+            obj.fields.each(function(f) {
+                console.log(f);
+                all += f.get('Name') + ' = models.' + djangoTypeMapping[f.get('Type')] + '()'
+            }, this);
+            
             return all;
-        }
+        },
     };
 
     g.registerGenerator('django', djangoGenerator);
