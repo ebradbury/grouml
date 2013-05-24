10ee7c6dc258d4e8f1915f0e2c33cdff493b6091
Elliot Bradbury <ebradbury@gmail.com>
2013-05-23 22:03:47 -0400

    removed toolbar code

--- js/init.js
+++ js/init.js
@@ -5,21 +5,12 @@ var GROUML = GROUML || {};
     GROUML.Events = _.extend({}, Backbone.Events);
 
     $(function() {
-        $.infinitedrag('#wall', {}, {
+        $.infinitedrag('#board', {}, {
             width: 100,
             height: 100,
             start_col: 0,
             start_row: 0,
             oncreate: function(){},
-        });
-
-        var $addButton = $('#add-class-diagram');
-
-        $addButton.button({
-            text: false,
-            icons: {
-                primary: "ui-icon-plusthick"
-            }
         });
 
         GROUML.Router = new GROUML.Routers.MainRouter();
